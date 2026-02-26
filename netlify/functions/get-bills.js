468ea3fcd1943c5147bdd736fac0055d8562ff14
carlosmalvarenga1-rgb <carlos.m.alvarenga1@gmail.com>
2026-02-26 12:39:04 -0700

    Update get-bills.js

--- netlify/functions/get-bills.js
+++ netlify/functions/get-bills.js
@@ -5,7 +5,7 @@ export async function handler(event) {
     // Defaults: 118th Congress, current bills, 50 results (we will raise this next)
     url.searchParams.set("format", "json");
     url.searchParams.set("congress", "118");
-    url.searchParams.set("limit", event.queryStringParameters?.limit || "50");
+    url.searchParams.set("limit", event.queryStringParameters?.limit || "250");
     url.searchParams.set("offset", event.queryStringParameters?.offset || "0");
     url.searchParams.set("api_key", process.env.CONGRESS_API_KEY || "");
 
